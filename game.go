680f524f6fb5eefe8ec0fb743218dc93e5b98443
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2020-03-28 21:47:51 -0500

    chore: detecting when game is won or lost

--- game.go
+++ game.go
@@ -1,19 +1,40 @@
 package main
 
-// Won indicates when the game is won by the player
+// Won indicates when the game has been won
 const Won = 1
+
+// Lost indicates when the game has been lost
+const Lost = 2
 
 // Board struct.
 type Board struct {
+	a [15]bool
 }
 
 // NewBoard creates a new Board.
-func NewBoard(i int) Board {
+func NewBoard(pegs ...int) Board {
 	b := Board{}
+	for _, i := range pegs {
+		b.a[i] = true
+	}
 	return b
 }
 
 // GameOver function.
 func GameOver(b Board) int {
-	return Won
+	if pegCount(b) == 1 {
+		return Won
+	} else {
+		return Lost
+	}
+}
+
+func pegCount(b Board) int {
+	count := 0
+	for i := 0; i < len(b.a); i++ {
+		if b.a[i] == true {
+			count = count + 1
+		}
+	}
+	return count
 }
--- game_test.go
+++ game_test.go
@@ -10,3 +10,8 @@ func TestUnitPlayerWinsWhenThereIsOnePegLeft(t *testing.T) {
 	b := NewBoard(1)
 	assert.Equal(t, Won, GameOver(b))
 }
+
+func TestUnitPlayerLosesWhenThereAreTwoDistantPegsLeft(t *testing.T) {
+	b := NewBoard(1, 4)
+	assert.Equal(t, Lost, GameOver(b))
+}
