18de0498dc5aad75212421da68b31a010d665693
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2020-04-12 11:00:29 -0500

    chore: tests for adjacency

--- game.go
+++ game.go
@@ -1,29 +1,89 @@
 package main
 
-// Won indicates when the game has been won
-const Won = 1
+// Result indicates the outcome of the game
+type Result int
 
-// Lost indicates when the game has been lost
-const Lost = 2
+const (
+	Won Result = 1 + iota
+	Lost
+	InProgress
+)
+
+// Direction type.
+type Direction pair
+
+// Directions.
+// The value of each Direction is a pair of numbers. 
+// The first value in the pair represents how row-values change
+//   as we move in that direction.
+// The second value in the pair represents how column-values change
+//   as we move in that direction.
+// Because of the topology of the board, there are six Directions:
+//   SE - Southeast (row- and column-values increase)
+//   NW - Northwest (row- and column-values decrease)
+//   SW - Southwest (row-values increase)
+//   NE - Northeast (row-values decrease)
+//   E  - East (column-values increase)
+//   W  - West (column-values decrease)
+var (
+	SE Direction = Direction{1, 1}
+	NW Direction = Direction{-1, -1}
+	SW Direction = Direction{1, 0}
+	NE Direction = Direction{-1, 0}
+	E Direction = Direction{0, 1}
+	W Direction = Direction{0, -1}
+)
+
+// rows is the number of rows on board.
+// rows is hard-coded to 5 since our board-size is fixed.
+const rows = 5
+
+// slots is the number of slots.
+// It can be defined using the formula for triangular numbers.
+const slots = rows * (rows + 1) / 2
 
 // Board struct.
 type Board struct {
-	a [15]bool
+	// This is a wasteful array: unused entries = rows * (rows - 1) / 2
+	a [rows][rows]bool
+}
+
+func (b *Board) fillSlot(loc int, peg bool) {
+	row, col := locToIndexes(loc)
+	b.a[row][col] = peg
+}
+
+func (b Board) getSlot(loc int) bool {
+	row, col := locToIndexes(loc)
+	return b.a[row][col]
+}
+
+func locToIndexes(loc int) (int, int) {
+	row := -1
+	for i := 1; i <= rows && row == -1; i++ {
+		if i*(i+1)/2 >= loc {
+			row = i - 1
+		}
+	}
+	col := loc - (row * (row + 1) / 2) - 1
+	return row, col
 }
 
 // NewBoard creates a new Board.
 func NewBoard(pegs ...int) Board {
 	b := Board{}
 	for _, i := range pegs {
-		b.a[i] = true
+		b.fillSlot(i, true)
 	}
 	return b
 }
 
 // GameOver function.
-func GameOver(b Board) int {
+func GameOver(b Board) Result {
 	if pegCount(b) == 1 {
 		return Won
+	} else if moveExists(b) {
+		return InProgress
 	} else {
 		return Lost
 	}
@@ -31,10 +91,23 @@ func GameOver(b Board) int {
 
 func pegCount(b Board) int {
 	count := 0
-	for i := 0; i < len(b.a); i++ {
-		if b.a[i] == true {
+	for i := 1; i <= slots; i++ {
+		if b.getSlot(i) {
 			count = count + 1
 		}
 	}
 	return count
+}
+
+func moveExists(b Board) bool {
+	return false
+}
+
+
+func adjacentSlots(b Board, d Direction) (int, int) {
+	return 2, 4
+}
+
+type pair struct {
+	row, col int
 }
--- game_test.go
+++ game_test.go
@@ -2,16 +2,51 @@ package main
 
 import (
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUnitPlayerWinsWhenThereIsOnePegLeft(t *testing.T) {
-	b := NewBoard(1)
-	assert.Equal(t, Won, GameOver(b))
+	for i := 1; i <= 15; i++ {
+		b := NewBoard(i)
+		assert.Equal(t, Won, GameOver(b))
+	}
 }
 
 func TestUnitPlayerLosesWhenThereAreTwoDistantPegsLeft(t *testing.T) {
 	b := NewBoard(1, 4)
 	assert.Equal(t, Lost, GameOver(b))
 }
+
+func TestLocToRowCol(t *testing.T) {
+	m := make(map[int]pair)
+	m[1] = pair{0, 0}
+	m[2] = pair{1, 0}
+	m[3] = pair{1, 1}
+	m[4] = pair{2, 0}
+	m[5] = pair{2, 1}
+	m[6] = pair{2, 2}
+	m[7] = pair{3, 0}
+	m[8] = pair{3, 1}
+	m[9] = pair{3, 2}
+	m[10] = pair{3, 3}
+	m[11] = pair{4, 0}
+	m[12] = pair{4, 1}
+	m[13] = pair{4, 2}
+	m[14] = pair{4, 3}
+	m[15] = pair{4, 4}
+
+	row, col := 0, 0
+	for k, v := range m {
+		row, col = locToIndexes(k)
+		assert.Equal(t, v.row, row)
+		assert.Equal(t, v.col, col)
+	}
+}
+
+func TestUnitAdjacentSlots(t *testing.T) {
+	b := NewBoard()
+	i, j := adjacentSlots(b, SW)
+	assert.Equal(t, 2, i)
+	assert.Equal(t, 4, j)
+}
+
